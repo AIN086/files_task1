f2dee6fa0c12ba72db030f3912b46c92711c8105
AIN086 <novozhilov952@gmail.com>
2022-04-21 23:37:39 +0500

    making changes

--- src/Main.java
+++ src/Main.java
@@ -6,61 +6,58 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String baseDirPath = "C://Games/";
+
+        String[] directoriesInGames = {"src", "res", "savegames", "temp"};
+        String[] directoriesInSrc = {"main", "test"};
+        String[] filesInMain = {"Main.java", "Utils.java"};
+        String[] directoriesInRes = {"drawables", "vectors", "icons"};
+
         StringBuilder sb = new StringBuilder();
 
-        File src = new File("C://Games/src" );
-        if (src.mkdir())
-            sb.append("Каталог " + src.getName() + " создан" + "\n");
-
-        File res = new File("C://Games/res" );
-        if (res.mkdir())
-            sb.append("Каталог " + res.getName() + " создан" + "\n");
-
-        File savegames = new File("C://Games/savegames" );
-        if (savegames.mkdir())
-            sb.append("Каталог " + savegames.getName() + " создан" + "\n");
-
-        File temp = new File("C://Games/temp" );
-        if (temp.mkdir())
-            sb.append("Каталог " + temp.getName() + " создан" + "\n");
-
-        File main = new File("C://Games/src/main" );
-        if (main.mkdir())
-            sb.append("Каталог " + main.getName() + " создан" + "\n");
-
-        File mainMain= new File("C://Games/src/main/Main.java" );
-        try {
-            if (mainMain.createNewFile())
-                sb.append("Файл " + mainMain.getName() + " был создан" + "\n");
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+        for (int j = 0; j < directoriesInGames.length; j++) {
+            String fullPath = baseDirPath + directoriesInGames[j];
+            File dir = new File(fullPath);
+            String message = "";
+            if (dir.mkdir())
+                message = "Каталог " + dir.getName() + " создан" + "\n";
+            sb.append(message);
         }
 
-        File test = new File("C://Games/test" );
-        if (test.mkdir())
-            sb.append("Каталог " + test.getName() + " создан" + "\n");
-
-        File mainUtils= new File("C://Games/src/main/Utils.java" );
-        try {
-            if (mainUtils.createNewFile())
-                sb.append("Файл " + mainUtils.getName() + " был создан" + "\n" );
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+        for (int j = 0; j < directoriesInSrc.length; j++) {
+            String fullPath = baseDirPath + directoriesInGames[0] + "/" + directoriesInSrc[j] + "/";
+            File dir = new File(fullPath);
+            String message = "";
+            if (dir.mkdir())
+                message = "Каталог " + dir.getName() + " создан" + "\n";
+            sb.append(message);
         }
 
-        File drawables = new File("C://Games/res/drawables" );
-        if (drawables.mkdir())
-            sb.append("Каталог " + drawables.getName()+ " создан" + "\n");
+        for (int j = 0; j < directoriesInRes.length; j++) {
+            String fullPath = baseDirPath + directoriesInGames[1] + "/" + directoriesInRes[j] + "/";
+            File dir = new File(fullPath);
+            String message = "";
+            if (dir.mkdir())
+                message = "Каталог " + dir.getName() + " создан" + "\n";
+            sb.append(message);
+        }
 
-        File vectors = new File("C://Games/res/vectors" );
-        if (vectors.mkdir())
-            sb.append("Каталог " + vectors.getName()+ " создан" + "\n");
 
-        File icons = new File("C://Games/res/icons" );
-        if (icons.mkdir())
-            sb.append("Каталог " + icons.getName()+ " создан" + "\n");
+        String fullDirectoryMainPath = baseDirPath + directoriesInGames[0] + "/" + directoriesInSrc[0] + "/";
+        for (int i = 0; i < directoriesInGames.length; i++) {
+            for (int j = 0; j < filesInMain.length; j++) {
+                String fullMainFileName = fullDirectoryMainPath + filesInMain[j];
+                File myFile = new File(fullMainFileName);
+                try {
+                    if (myFile.createNewFile())
+                        sb.append("Файл " + myFile.getName() + " был создан" + "\n");
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }
 
-        File tempFile= new File("C://Games/temp/temp.txt" );
+        File tempFile = new File("C://Games/temp/temp.txt");
         try {
             if (tempFile.createNewFile())
                 sb.append("Файл " + tempFile.getName() + " был создан" + "\n");
@@ -71,8 +68,9 @@ public class Main {
         try (FileWriter writer = new FileWriter("C://Games/temp/temp.txt", false)) {
             writer.write(String.valueOf(sb));
             writer.flush();
-        } catch (IOException ex)
-        { System.out.println(ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
         }
     }
 }
+
